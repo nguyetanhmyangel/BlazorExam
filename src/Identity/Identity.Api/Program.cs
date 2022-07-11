7b4a5384c7db0fbde96a9df33e6335ad1f6ac960
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-07-11 20:29:21 +0700

    5th commit

--- src/Identity/Identity.Api/Program.cs
+++ src/Identity/Identity.Api/Program.cs
@@ -1,100 +1,96 @@
-using System.Reflection;
 using Identity.Api.Database;
-using Identity.Api.Models;
-using Identity.Api.Services;
-using IdentityServer4.Services;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using IdentityServer4.EntityFramework.DbContexts;
+using Serilog;
+using System.Reflection;
+using Identity.Api.Extensions;
+using Microsoft.Extensions.Options;
 
+namespace Identity.Api;
 
-var builder = WebApplication.CreateBuilder(args);
-IConfiguration configuration = builder.Configuration;
-
-
-string? migrationsAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
-
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(connectionString,
-        sqlServerOptionsAction: sqlOptions =>
-        {
-            sqlOptions.MigrationsAssembly(migrationsAssembly);
-            sqlOptions.EnableRetryOnFailure(
-                maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(30),
-                errorNumbersToAdd: null);
-        }));
-
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-//https://identityserver4.readthedocs.io/en/latest/reference/options.html
-builder.Services.AddIdentityServer(x =>
-            {
-                x.IssuerUri = "https://tedu.com.vn";
-                x.Authentication.CookieLifetime = TimeSpan.FromHours(2);
-            })
-            .AddDeveloperSigningCredential()
-            .AddAspNetIdentity<ApplicationUser>()
-            .AddConfigurationStore(options =>
-            {
-                options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
-                    sqlServerOptionsAction: sqlOptions =>
-                    {
-                        sqlOptions.MigrationsAssembly(migrationsAssembly);
-                        sqlOptions.EnableRetryOnFailure(
-                            maxRetryCount: 5,
-                            maxRetryDelay: TimeSpan.FromSeconds(30),
-                            errorNumbersToAdd: null);
-                    });
-            })
-            .AddOperationalStore(options =>
-            {
-                options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
-                    sqlServerOptionsAction: sqlOptions =>
-                    {
-                        sqlOptions.MigrationsAssembly(migrationsAssembly);
-                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 5,
-                            maxRetryDelay: TimeSpan.FromSeconds(30),
-                            errorNumbersToAdd: null);
-                    });
-            })
-            .Services.AddTransient<IProfileService, ProfileService>();
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static int Main(string[] args)
+    {
+        string? appName = typeof(Startup).Namespace;
+
+        var configuration = GetConfiguration();
+
+        Log.Logger = CreateSerilogLogger(configuration);
+
+        try
+        {
+            Log.Information("Configuring web host ({ApplicationContext})...", appName);
+            var host = BuildWebHost(configuration, args);
+
+            Log.Information("Applying migrations ({ApplicationContext})...", appName);
+            host.MigrateDbContext<PersistedGrantDbContext>((_, __) => { })
+                .MigrateDbContext<ApplicationDbContext>((context, services) =>
+                {
+                    var env = services.GetService<IWebHostEnvironment>();
+                    var logger = services.GetService<ILogger<ApplicationDbContextSeed>>();
+                    var settings = services.GetService<IOptions<AppSettings>>();
+
+                    new ApplicationDbContextSeed()
+                        .SeedAsync(context, env, logger, settings)
+                        .Wait();
+                })
+                .MigrateDbContext<ConfigurationDbContext>((context, services) =>
+                {
+                    new ConfigurationDbContextSeed()
+                        .SeedAsync(context, configuration)
+                        .Wait();
+                });
+
+            Log.Information("Starting web host ({ApplicationContext})...", appName);
+            host.Run();
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", appName);
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+
+        IHost BuildWebHost(IConfiguration configuration, string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.CaptureStartupErrors(false);
+                webBuilder.ConfigureAppConfiguration(x => x.AddConfiguration(configuration));
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                webBuilder.UseSerilog();
+            })
+            .Build();
+
+        Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", appName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddUserSecrets(Assembly.GetAssembly(typeof(Startup)));
+
+            var config = builder.Build();
+            return builder.Build();
+        }
+    }
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseRouting();
-app.UseIdentityServer();
-
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-//app.MapControllers();
-
-app.Run();
