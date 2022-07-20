5a5ad8bb874dffd459202139db8d50992692c5fb
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-07-20 16:31:13 +0700

    20th commit

---
+++ src/Examination/Examination.Api/Startup.cs
@@ -0,0 +1,83 @@
+using Examination.Application.Commands.StartExam;
+using Examination.Application.Mapping;
+using Examination.Domain.AggregateModels.ExamAggregate;
+using Examination.Domain.AggregateModels.ExamResultAggregate;
+using Examination.Domain.AggregateModels.UserAggregate;
+using Examination.Infrastructure.Repositories;
+using Examination.Infrastructure.SeedWorks;
+using MediatR;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+
+namespace Examination.Api;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IMongoClient>(c =>
+        {
+            var user = Configuration.GetValue<string>("DatabaseSettings:User");
+            var password = Configuration.GetValue<string>("DatabaseSettings:Password");
+            var server = Configuration.GetValue<string>("DatabaseSettings:Server");
+            var databaseName = Configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+            return new MongoClient(
+                "mongodb://" + user + ":" + password + "@" + server + "/" + databaseName + "?authSource=admin");
+        });
+        services.AddScoped(c => c.GetService<IMongoClient>()?.StartSession());
+        services.AddAutoMapper(cfg => { cfg.AddProfile(new MappingProfile()); });
+        services.AddMediatR(typeof(StartExamCommandHandler).Assembly);
+        services.AddControllers();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+        });
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Examination.API", Version = "v1" });
+        });
+        services.Configure<ExamSettings>(Configuration);
+
+        services.AddTransient<IExamRepository, ExamRepository>();
+        services.AddTransient<IExamResultRepository, ExamResultRepository>();
+        services.AddTransient<IUserRepository, UserRepository>();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Examination.API v1"));
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseCors("CorsPolicy");
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
+
