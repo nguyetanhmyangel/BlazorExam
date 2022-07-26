2c49ff779666fb84df41462f814a089580b38d14
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-07-26 15:49:51 +0700

    28th commit

--- src/Examination/Examination.Application/Commands/v1/StartExam/StartExamCommandHandler.cs
+++ src/Examination/Examination.Application/Commands/v1/StartExam/StartExamCommandHandler.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Examination.Domain.AggregateModels.ExamResultAggregate;
 using MediatR;
 
--- src/Examination/Examination.Domain/AggregateModels/CategoryAggregate/Category.cs
+++ src/Examination/Examination.Domain/AggregateModels/CategoryAggregate/Category.cs
@@ -5,9 +5,11 @@ namespace Examination.Domain.AggregateModels.CategoryAggregate;
 
 public class Category : Entity
 {
+    public Category(string id, string name, string urlPath) => (Id, Name, UrlPath) = (id, name, urlPath);
+    
     [BsonElement("name")]
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     [BsonElement("urlPath")]
-    public string? UrlPath { set; get; } //domain/exam-category-1/
+    public string UrlPath { set; get; } //domain/exam-category-1/
 }
